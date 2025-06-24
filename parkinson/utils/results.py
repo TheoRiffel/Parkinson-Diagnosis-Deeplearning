cff1f40ba88621accb33ab2a29639a469cea01d0
Gabriel Merlin <gabrielcmerlin@gmail.com>
2025-06-24 18:09:11 -0300

    Finalizando 3_timeseries

--- parkinson/utils/results.py
+++ parkinson/utils/results.py
@@ -1,7 +1,29 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 def plot_losses(train_loss, val_loss):
-    plt.plot(np.arange(len(train_loss)), train_loss, color='b', label='train')
-    plt.plot(np.arange(len(val_loss)), val_loss, color='r', label='val')
-    plt.legend()
+    fig, ax = plt.subplots()
+    ax.plot(np.arange(len(train_loss)), train_loss, color='b', label='train')
+    ax.plot(np.arange(len(val_loss)), val_loss, color='r', label='val')
+    ax.legend()
+    return fig
+
+def plot_confusion_matrix(preds, labels, class_names=None):
+    cm = confusion_matrix(labels, preds)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=class_names)
+    fig, ax = plt.subplots(figsize=(5,5))
+    disp.plot(ax=ax, cmap='Blues', colorbar=False)
+    plt.title("Matriz de Confusão")
+    return fig
+
+def metrics_to_dataframe(metrics):
+    data = {
+        'Acurácia': [metrics['acc']],
+        'F1': [metrics['f1']],
+        'Precisão': [metrics['precision']],
+        'Recall': [metrics['recall']]
+    }
+
+    print(pd.DataFrame(data).to_string(index=False))
